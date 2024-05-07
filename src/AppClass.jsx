ac11c2d6c8592ac1a287d32461ff83880f22f018
Sweatha <sweathadharan1212@gmail.com>
2024-05-07 16:48:14 +0530

    first commit

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,48 @@
-import { Component } from "react";
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant
       },
       {
-        id:4,
-        img:elephant
+        id: 4,
+        img: elephant
       }
-    ]
+    ];
     return data;
-  }
+  };
 
-  // code here
+  render() {
+    // code here
+    const images = this.imageData();
+
+    const imageElements = [];
+    for (let i = 0; i < images.length; i++) {
+      imageElements.push(
+        <img key={images[i].id} src={images[i].img} alt={`Image ${images[i].id}`} />
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Kalvium gallary</h1>
+        <div className="image-container">
+          {imageElements}
+        </div>
+      </div>
+    );
+  }
 }
